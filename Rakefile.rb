b92ccf7a06399ab161547f5dca7f175eff972add
yaroslav <yaroslavmaslov0951@gmail.com>
2023-10-31 13:36:50 +0300

    Feature description

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,19 +1,32 @@
 # Rakefile
 
-require_relative 'test_data' # Импортируем файл с тестовыми данными
+require_relative 'test_data'
 
 namespace :docker do
-  desc "Скачать Docker образ"
-  task :pull_image do
+  desc "Download a Docker image and start a container"
+  task :pull_and_run_container do
     version = TestData[:version] || 'latest'
     image_name = "onlyoffice/4testing-documentserver-ee:#{version}"
+
+
     sh "docker pull #{image_name}"
+
+
+    sh "docker run -d --name my_container  #{image_name}"
   end
 
-  desc "Удалить Docker образ"
+  desc "Remove a Docker image, stopping and deleting the associated container"
   task :remove_image do
     version = TestData[:version] || 'latest'
     image_name = "onlyoffice/4testing-documentserver-ee:#{version}"
+
+
+    sh "docker stop my_container" rescue nil
+
+
+    sh "docker rm my_container" rescue nil
+
+
     sh "docker rmi #{image_name}"
   end
 end
