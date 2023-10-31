4aec3cbfd1e50bc7fd1a577604a78301913ee2c9
yaroslav <yaroslavmaslov0951@gmail.com>
2023-10-31 13:16:37 +0300

    Revert "added the ability to lift the container after loading"
    This reverts commit c41427a80f4b6f1b8d8ad4f128dc310365e01967.

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # Ruby-Rake-script
-Rake script for downloading the assembly and removing docker docs ee, regardless of version. 
-
-And in a separate file there is an implemented test date that is imported into the rake script. 
-
-Downloal command: rake docker:pull_and_run_container --trace. 
-
-Removing command: rake docker:remove_image[Version]. 
-
+Rake script for downloading the assembly and removing docker docs ee, regardless of version
+And in a separate file there is an implemented test date that is imported into the rake script
+Downloal command: rake docker:pull_image[Version]
+Removing command: rake docker:remove_image[Version]
 If the version is not specified, the default value (latest) will be used.
--- Rakefile.rb
+++ Rakefile.rb
@@ -1,32 +1,19 @@
 # Rakefile
 
-require_relative 'test_data'
+require_relative 'test_data' # Импортируем файл с тестовыми данными
 
 namespace :docker do
-  desc "Download a Docker image and start a container"
-  task :pull_and_run_container do
+  desc "Скачать Docker образ"
+  task :pull_image do
     version = TestData[:version] || 'latest'
     image_name = "onlyoffice/4testing-documentserver-ee:#{version}"
-
-
     sh "docker pull #{image_name}"
-
-
-    sh "docker run -d --name my_container  #{image_name}"
   end
 
-  desc "Remove a Docker image, stopping and deleting the associated container"
+  desc "Удалить Docker образ"
   task :remove_image do
     version = TestData[:version] || 'latest'
     image_name = "onlyoffice/4testing-documentserver-ee:#{version}"
-
-
-    sh "docker stop my_container" rescue nil
-
-
-    sh "docker rm my_container" rescue nil
-
-
     sh "docker rmi #{image_name}"
   end
 end
