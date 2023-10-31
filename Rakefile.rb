7365be5db79bc390d85bc54a1f13f82c31a19c23
yaroslav <yaroslavmaslov0951@gmail.com>
2023-10-31 14:04:34 +0300

    .

---
+++ Rakefile.rb
@@ -0,0 +1,32 @@
+# Rakefile
+
+require_relative 'test_data'
+
+namespace :docker do
+  desc "Download a Docker image and start a container"
+  task :pull_and_run_container do
+    version = TestData[:version] || 'latest'
+    image_name = "onlyoffice/4testing-documentserver-ee:#{version}"
+
+
+    sh "docker pull #{image_name}"
+
+
+    sh "docker run -d --name my_container  #{image_name}"
+  end
+
+  desc "Remove a Docker image, stopping and deleting the associated containers"
+  task :remove_image do
+    version = TestData[:version] || 'latest'
+    image_name = "onlyoffice/4testing-documentserver-ee:#{version}"
+
+
+    sh "docker stop my_container" rescue nil
+
+
+    sh "docker rm my_container" rescue nil
+
+
+    sh "docker rmi #{image_name}"
+  end
+end
