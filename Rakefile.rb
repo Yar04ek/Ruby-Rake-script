80e4942490178a8184f8aa6ce8e042ae9ba4346e
Yarosalv Maslov <148754748+Yar04ek@users.noreply.github.com>
2023-11-07 13:32:53 +0300

    removed Russian words

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,17 +1,17 @@
 require_relative 'test_data'
 
 namespace :docker do
-  desc "Скачать Docker образ"
+  desc "Download Docker image"
   task :pull_image do
     version = TestData[:version] || 'latest'
     image_name = "onlyoffice/4testing-documentserver-ee:#{version}"
-    ports = TestData[:ports] || 'YOUR_DEFAULT_PORTS' # Замените 'YOUR_DEFAULT_PORTS' на фактическую конфигурацию порта по умолчанию
+
 
     sh "docker pull #{image_name}"
     sh "docker run -it -d -p 80:80 --name my_container #{image_name}"
   end
 
-  desc "Удалить Docker контейнер и образ"
+  desc "Remove Docker container and image"
   task :remove_container_and_image do
     sh "docker stop my_container"
     sh "docker rm my_container"
