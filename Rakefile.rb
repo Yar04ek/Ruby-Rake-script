8063e9a6008e3c14c3faeec829beb48af8092787
Yarosalv Maslov <148754748+Yar04ek@users.noreply.github.com>
2023-11-07 13:29:37 +0300

    new method added

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,32 +1,22 @@
-# Rakefile
-
 require_relative 'test_data'
 
 namespace :docker do
-  desc "Download a Docker image and start a container"
-  task :pull_and_run_container do
+  desc "Скачать Docker образ"
+  task :pull_image do
     version = TestData[:version] || 'latest'
     image_name = "onlyoffice/4testing-documentserver-ee:#{version}"
-
+    ports = TestData[:ports] || 'YOUR_DEFAULT_PORTS' # Замените 'YOUR_DEFAULT_PORTS' на фактическую конфигурацию порта по умолчанию
 
     sh "docker pull #{image_name}"
-
-
-    sh "docker run -d --name my_container  #{image_name}"
+    sh "docker run -it -d -p 80:80 --name my_container #{image_name}"
   end
 
-  desc "Remove a Docker image, stopping and deleting the associated containers"
-  task :remove_image do
+  desc "Удалить Docker контейнер и образ"
+  task :remove_container_and_image do
+    sh "docker stop my_container"
+    sh "docker rm my_container"
     version = TestData[:version] || 'latest'
     image_name = "onlyoffice/4testing-documentserver-ee:#{version}"
-
-
-    sh "docker stop my_container" rescue nil
-
-
-    sh "docker rm my_container" rescue nil
-
-
     sh "docker rmi #{image_name}"
   end
 end
