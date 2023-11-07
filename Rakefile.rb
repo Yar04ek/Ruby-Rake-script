014e713e0a031e9334798cdf7fdafb7bd329ea3b
Yarosalv Maslov <148754748+Yar04ek@users.noreply.github.com>
2023-11-07 14:01:25 +0300

    added ENV method

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,32 +1,21 @@
-# Rakefile
-
 require_relative 'test_data'
 
 namespace :docker do
-  desc "Download a Docker image and start a container"
-  task :pull_and_run_container do
-    version = TestData[:version] || 'latest'
+  desc "Download Docker image"
+  task :pull_image do
+    version = ENV.fetch('DOCKER_IMAGE_VERSION', 'latest')
     image_name = "onlyoffice/4testing-documentserver-ee:#{version}"
-
 
     sh "docker pull #{image_name}"
-
-
-    sh "docker run -d --name my_container  #{image_name}"
+    sh "docker run -it -d -p 80:80 --name my_container #{image_name}"
   end
 
-  desc "Remove a Docker image, stopping and deleting the associated containers"
-  task :remove_image do
-    version = TestData[:version] || 'latest'
+  desc "Remove Docker container and image"
+  task :remove_container_and_image do
+    sh "docker stop my_container"
+    sh "docker rm my_container"
+    version = ENV.fetch('DOCKER_IMAGE_VERSION', 'latest')
     image_name = "onlyoffice/4testing-documentserver-ee:#{version}"
-
-
-    sh "docker stop my_container" rescue nil
-
-
-    sh "docker rm my_container" rescue nil
-
-
     sh "docker rmi #{image_name}"
   end
 end
